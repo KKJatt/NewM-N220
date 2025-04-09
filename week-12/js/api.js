5cda6e66b049d5bb281b6663a0014c9f2abbef83
Karanjot Kailay <KaranjotKailay@gmail.com>
2025-04-09 16:31:18 -0400

    Showing Correct Digimon

---
+++ week-12/js/api.js
@@ -0,0 +1,51 @@
+// https://digimon-api.vercel.app/api/digimon
+
+const digimonListRef = document.querySelector("#digimon-list");
+
+async function getDigimon() {
+//   fetch("https://digimon-api.vercel.app/api/digimon")
+//     .then(function (digiResp) {
+//       return digiResp.json();
+//     })
+//     .then((digiData) => {
+//       console.log(digiData);
+//     })
+//     .catch(function (err) {
+//       console.warn(err);
+//     })
+//     .finally(() => {
+//       console.log("Fetch Complete!");
+//       // code to cleanup => do it here
+//     });
+
+try {
+    const digiResp = await fetch("https://digimon-api.vercel.app/api/digimon");
+
+    const digiData = await digiResp.json();
+
+    digimonListRef.innerHTML = "";
+    
+    for(let i = 0; i<digiData.length;i++){
+        const currentDigimon = digiData[i];
+
+        const newCard = document.createElement("div")
+        newCard.classList.add("digimon-card");
+        newCard.innerHTML += `
+        <img src="${currentDigimon.img}" alt="${currentDigimon.name}">
+        <h4>${currentDigimon.name}</h4>
+        <button class="like">&hearts;</button>
+        `;
+
+        digimonListRef.appendChild(newCard)
+    }
+} catch (e){
+console.warn(e)
+}
+
+
+    console.log("End of function");
+}
+
+
+getDigimon();
+
